3267727a114f3a73145aa156e39d40a941b3f289
jsdevper <outhkl9716@outlook.com>
2023-07-07 21:58:17 +0900

    .

--- src/components/ConchImg.tsx
+++ src/components/ConchImg.tsx
@@ -1,5 +1,5 @@
 "use client";
 import Image from "next/image";
 export default function ConchImg() {
-    return <div><Image className="w-auto h-auto" src="/images/conch.png" alt="소라고동 이미지" width={40} height={40} /></div>
+    return <div><Image priority={true} className="w-auto h-auto" src="/images/conch.png" alt="소라고동 이미지" width={40} height={40} /></div>
 }
